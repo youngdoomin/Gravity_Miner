53a0b58d623147d029e644c5d2aff0e0273341aa
lemetoa <32997772+lemetoa@users.noreply.github.com>
2020-08-19 20:28:12 +0900

    Revert "Revert "버그 수정 0.4""
    This reverts commit 778ca45dcb57846815e667bb8bb7e395c47dae25.

--- Assets/Scripts/SpawnOrDel/Item_Spawn.cs
+++ Assets/Scripts/SpawnOrDel/Item_Spawn.cs
@@ -1,18 +1,26 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class Item_Spawn : MonoBehaviour
 {
+    GameObject item;
     //public GameObject spawnPrefab;
     //private GameObject Jchild;
     // Start is called before the first frame update
     void Start()
     {
+        item = gameObject.transform.GetChild(1).gameObject;
         //SpawnJam();
     }
 
-    void SpawnJam()
+    IEnumerator SpawnJam()
     {
-        gameObject.transform.GetChild(1).gameObject.SetActive(true);
+        item.SetActive(true);
+        item.GetComponent<BoxCollider2D>().enabled = false;
+        yield return new WaitForSeconds(0.1f);
+        item.GetComponent<BoxCollider2D>().enabled = true;
+
+        //gameObject.transform.GetChild(1).gameObject.transform.localScale = new Vector3(1, 1, 0);
         //Jchild = Instantiate(spawnPrefab, transform.position, Quaternion.identity); //게임옵젝생성(오브젝트, 위치, 회전)       identity 기본값
         //Jchild.transform.SetParent(this.gameObject.transform.parent);
     }
